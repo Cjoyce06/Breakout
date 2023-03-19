01a00e94136b92336f860f0c3fb77d830d478aa7
Colin Joyce <Cjoyce1@bsd220.org>
2023-03-18 19:25:58 -0500

    Created background

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -14,6 +14,21 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
+        createBackround()
+    }
+    
+    func createBackround() {
+        let stars = SKTexture(imageNamed: "Stars")
+        for i in 0...1{
+            let starsBackround = SKSpriteNode(texture: stars)
+            starsBackround.zPosition = -1
+            starsBackround.position = CGPoint(x: 0, y: starsBackround.size.height * CGFloat(i))
+            addChild(starsBackround)
+            let moveDown = SKAction.moveBy(x: 0, y: -starsBackround.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: starsBackround.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            starsBackround.run(moveForever)
+        }
     }
 }
