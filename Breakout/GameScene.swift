baa2b3416d14c6cfc1a5f133130911665a84eab1
Colin Joyce <Cjoyce1@bsd220.org>
2023-03-23 20:27:08 -0500

    Set up multiple rows of multicolored bricks

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -67,10 +67,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 contact.bodyB.node == brick {
                 score += 1
                 updateLables()
+                if brick.color == .blue {
+                    brick.color = .orange
+                }
+                else if brick.color == .orange {
+                    brick.color = .green
+                }
+                else{
                 brick.removeFromParent()
                 removedBricks += 1
-                if removedBricks == bricks.count {
-                    gameOver(winner: true)
+                    if removedBricks == bricks.count {
+                        gameOver(winner: true)
+                    }
                 }
             }
         }
@@ -214,12 +222,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         bricks.removeAll()
         removedBricks = 0
+        
         let count = Int(frame.width) / 55
         let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int(frame.minX) + 25
-        let y = Int(frame.maxY) - 65
-        for i in 0..<count {
-            let x = i * 55 + xOffset
-            makeBrick(x: x, y: y, color: .green)
+        let colors: [UIColor] = [.blue, .orange, .green]
+        for r in 0..<3 {
+            let y = Int(frame.maxY) - 65 - (r * 25)
+            for i in 0..<count {
+                let x = i * 55 + xOffset
+                makeBrick(x: x, y: y, color: colors[r])
+            }
         }
     }
 }
